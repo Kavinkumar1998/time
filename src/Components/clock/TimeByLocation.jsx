1dad5d74ef23d6d7b72cbc7b2cec05a3aa885f56
Kavinkumar1998 <kavinajith412@gmail.com>
2024-10-28 11:51:46 +0530

    weather added to timebylocation

--- src/Components/clock/TimeByLocation.jsx
+++ src/Components/clock/TimeByLocation.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate,useParams  } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCoordinates, getTime } from '../Redux/Slice'; // Import the actions from your Redux slice
+import { getCoordinates, getTime, getWeather } from '../Redux/Slice'; // Import the actions from your Redux slice
 import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet'
 const TimeByLocation = () => {
   const [location, setLocation] = useState({
@@ -19,7 +19,7 @@ const TimeByLocation = () => {
   const dispatch = useDispatch();
   const { city } = useParams(); 
   // Get state data from Redux
-  const { Time, Location, isLoading, error: reduxError } = useSelector((state) => state.time);
+  const { Time, Location,Weather, isLoading, error: reduxError } = useSelector((state) => state.time);
 
 
 
@@ -51,14 +51,13 @@ const TimeByLocation = () => {
     if (Location?.data?.length > 0) {
       const coordinates = {
         latitude: Location.data[0].lat,
-        longitude: Location.data[0].lon
+        longitude: Location.data[0].lon,
       };
-      const positionData =[Location.data[0].lat, Location.data[0].lon]
-      setPosition(positionData)
-      console.log('Coordinates:', coordinates);  // For debugging
-      dispatch(getTime(coordinates));  // Dispatch the lat/lon to get the time
+      setPosition([coordinates.latitude, coordinates.longitude]);
+      dispatch(getTime(coordinates));
+      dispatch(getWeather(coordinates));
     }
-  }, [Location, dispatch]);  // Location is a dependency, so the effect will run when Location changes
+  }, [Location, dispatch]);
 
   // Update local time every second
   useEffect(() => {
@@ -91,7 +90,32 @@ const TimeByLocation = () => {
       setWeekNumber(week);
     }
   }, [currentLocalTime]);
+  const WeatherData = Weather?.data ?.current|| {};
+  const { dewpoint_c,feelslike_c,heatindex_c,humidity,pressure_mb,temp_c,uv,vis_km,wind_degree,wind_dir,wind_kph}= WeatherData
+  console.log(WeatherData)
+ const weatherCondition = Weather?.data ?.current.condition|| {};
+ const {text,icon}=weatherCondition
+  const windDirectionsToDegrees = {
+    N: 0,
+    NNE: 22.5,
+    NE: 45,
+    ENE: 67.5,
+    E: 90,
+    ESE: 112.5,
+    SE: 135,
+    SSE: 157.5,
+    S: 180,
+    SSW: 202.5,
+    SW: 225,
+    WSW: 247.5,
+    W: 270,
+    WNW: 292.5,
+    NW: 315,
+    NNW: 337.5
+  };
 
+  // Retrieve the rotation angle for the current wind direction
+  const windRotationAngle = windDirectionsToDegrees[wind_dir] || 0;
   return (
     <div className="pt-8">
   
@@ -141,6 +165,43 @@ const TimeByLocation = () => {
          <span className='text-4xl font-bold text-white'>Time in {city} now :</span>
          <p className="text-9xl font-bold text-white">{new Date(currentLocalTime).toLocaleTimeString()}</p>
          <span className='text-4xl font-bold text-white'>{formattedDate}, week {weekNumber}</span>
+         <div>
+          <span className='text-4xl font-bold text-white'>
+            WEATHER in {city} now:
+          </span>
+          <div>
+          <span className='text-xl font-bold text-white'>{temp_c}°C Feels like {feelslike_c}°C</span>
+            </div>
+            <div>
+            <div className="flex justify-center items-center space-x-2">
+  <img src={icon} alt="Weather icon" className="w-8 h-8" />
+  <span className="text-xl font-bold text-white">{text}</span>
+</div>
+            </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+          <div  className="border rounded-lg p-8">         
+              <span className='text-xl font-bold text-white'>Humidity :{humidity}% </span>
+               </div>
+               <div  className="border rounded-lg p-8">         
+              <span className='text-xl font-bold text-white'>Pressure : {pressure_mb} mb</span>
+               </div>
+               <div  className="border rounded-lg p-8">         
+              <span className='text-xl font-bold text-white'>Visibility :{vis_km} km </span>
+               </div>
+               <div  className="border rounded-lg p-8">         
+              <span className='text-xl font-bold text-white'>Dewpoint : {dewpoint_c}°C</span>
+               </div>
+               <div  className="border rounded-lg p-8">         
+              <span className='text-xl font-bold text-white'>Wind : {wind_kph} km/h {wind_dir}<span className="ml-2 text-2xl font-bold text-white"
+              style={{ display: "inline-block", transform: `rotate(${windRotationAngle}deg)` }}>
+          ↑
+        </span></span>
+               </div>
+               <div  className="border rounded-lg p-8">         
+              <span className='text-xl font-bold text-white'>UV Index : {uv} </span>
+               </div>
+            </div>
+          </div>
           </div>
           
 
